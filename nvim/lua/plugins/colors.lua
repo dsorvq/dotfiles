5c2c7980744c36ce6474233f83bef9ac30c624c1
dsorvq <dsorvq@gmail.com>
2024-09-18 22:17:23 +0300

    Change colorscheme to github dark default

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,12 +1,12 @@
 return {
   {
-    "zenbones-theme/zenbones.nvim",
-    dependencies = { "rktjmp/lush.nvim" },
+    "projekt0n/github-nvim-theme",
+    -- dependencies = { "rktjmp/lush.nvim" },
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd([[set background=dark]])
-      vim.cmd([[colorscheme zenbones]])
+      -- vim.cmd([[set background=dark]])
+      vim.cmd([[colorscheme github_dark_default]])
     end,
   }
 }
