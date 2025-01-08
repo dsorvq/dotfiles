28dd1b77fc791f664592ad59016c6ef8f05e6f89
dsorvq <138303821+dsorvq@users.noreply.github.com>
2025-01-08 04:16:11 +0300

    Add fzf extension to telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,17 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function() 
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
+  config = function()
+    local telescope = require('telescope')
     local builtin = require('telescope.builtin')
+
+    telescope.load_extension('fzf')
+
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-  end
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+  end,
 }
