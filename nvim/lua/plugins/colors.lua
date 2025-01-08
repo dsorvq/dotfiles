1165e8341a52d64c6b1a4cc1f1ac08f62de26c63
dsorvq <138303821+dsorvq@users.noreply.github.com>
2025-01-08 04:04:31 +0300

    More lua based approach

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,12 +1,10 @@
 return {
   {
     "projekt0n/github-nvim-theme",
-    -- dependencies = { "rktjmp/lush.nvim" },
     lazy = false,
     priority = 1000,
     config = function()
-      -- vim.cmd([[set background=dark]])
-      vim.cmd([[colorscheme github_dark_high_contrast]])
+      vim.cmd.colorscheme("github_dark_high_contrast")
     end,
   }
 }
