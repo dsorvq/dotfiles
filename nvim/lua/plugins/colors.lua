63cb2ca4f46578e93e13eaa904313173193e08fb
= <=>
2025-01-17 19:14:08 +0300

    Update colorscheme

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,10 +1,11 @@
 return {
   {
-    "projekt0n/github-nvim-theme",
+    "nyoom-engineering/oxocarbon.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("github_dark_high_contrast")
+      vim.opt.background = "dark"
+      vim.cmd.colorscheme("oxocarbon")
     end,
   }
 }
