dffdd3a4dbb4f51176074753b1828d7c6800b81c
dsorvq <dsorvq@gmail.com>
2026-03-23 01:09:36 +0300

    Update colours

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,33 +1,11 @@
 return {
-  --[[
   {
-    -- "ellisonleao/gruvbox.nvim",
-    -- "shaunsingh/nord.nvim",
-    "jpwol/thorn.nvim",
+    "projekt0n/github-nvim-theme",
     lazy = false,
     priority = 1000,
     config = function()
-      require("thorn").setup({
-        theme = dark, -- 'light' or 'dark' - defaults to vim.o.background if unset
-        background = "warm", -- options are 'warm' and 'cold'
-
-        styles = {
-          keywords = { italic = false },
-          comments = { italic = false },
-          strings  = { italic = false },
-          variables  = { italic = false },
-          functions = { italic = false },
-          parameters = { italic = false },
-        },
-      })
-      -- TODO: have no idea how to properly disalbe italic, but this works
-      vim.api.nvim_set_hl(0, "@function.builtin.lua", { italic = false })
-      vim.api.nvim_set_hl(0, "@function.builtin", { italic = false })
-
-      -- vim.g.nord_italic = false -- for nord
-      vim.opt.background = "dark"
-      vim.cmd.colorscheme("thorn")
+      -- vim.opt.background = "dark"
+      vim.cmd.colorscheme("github_dark_default")
     end,
   }
-  --]]
 }
