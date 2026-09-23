f30ebdad08d28104487a904ea90a2e9610d913a7
dsorvq <dsorvq@gmail.com>
2026-09-23 16:05:23 +0300

    Migrate Treesitter setup for Neovim 0.12

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,27 @@
+local languages = { "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" }
+
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
-  enabled = true,
   config = function()
-    require'nvim-treesitter.configs'.setup {
-      ensure_installed = {
-	"c", "cpp",
-	"lua", "vim", "vimdoc",
-	"query", "markdown", "markdown_inline"
-      },
-      sync_install = false,
-      auto_install = false,
+    local treesitter = require("nvim-treesitter")
 
-      highlight = {
-	enable = true,
-	-- Disable treesitter highlight for large files
-	disable = function(lang, buf)
-	  local max_filesize = 100 * 1024 -- 100 KB
-	  local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-	  if ok and stats and stats.size > max_filesize then
-	    return true
-	  end
-	end,
+    -- Keep parsers installed by the previous nvim-treesitter setup available.
+    treesitter.setup({ install_dir = vim.fn.stdpath("data") .. "/lazy/nvim-treesitter" })
+    treesitter.install(languages)
 
-	additional_vim_regex_highlighting = false,
-      },
-    }
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "c", "cpp", "lua", "vim", "help", "query", "markdown" },
+      callback = function(args)
+        local path = vim.api.nvim_buf_get_name(args.buf)
+        local stats = path ~= "" and vim.uv.fs_stat(path)
+        if stats and stats.size > 100 * 1024 then
+          return
+        end
+        vim.treesitter.start(args.buf)
+      end,
+    })
   end,
 }
